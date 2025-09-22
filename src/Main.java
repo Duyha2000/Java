895f0f0f1f965d6e8ee92ca00f26e7209301d1e0
mac0s <tranduy10a@gmail.com>
2025-09-22 11:02:06 +0700

    heap

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.util.Arrays;
 
 public class Main {
-
+    // https://docs.google.com/document/d/1V2uhLBNN_ITpXO2eL9MTpHiFdcwVnWSNZ4J6JV1zAvs/edit?tab=t.4swdymkap22l
+    // https://code-with-me.global.jetbrains.com/G7Mlq_j9Rqi-c6lUQPXt9Q#p=IU&fp=DA1DF37C7714832CF1C0A16034364EB78DEABA83B01C07E51847084BA85B70D0&newUi=true
+    // https://meet.google.com/hzq-wjgp-wyj
+    // https://docs.google.com/spreadsheets/d/1AHleYM1JweYpHPo8dx8kWsNQCkXjRfn-yRd7HVzWBXo/edit?gid=704761184#gid=704761184
     static class MaxHeap {
         int[] a;
         int size;
@@ -17,12 +20,6 @@ public class Main {
             buildMaxHeap();
         }
 
-        static void swap(int[] x, int i, int j) {
-            int t = x[i];
-            x[i] = x[j];
-            x[j] = t;
-        }
-
         // Xây max-heap từ mảng hiện có: O(n)
         void buildMaxHeap() {
             for (int i = (size / 2) - 1; i >= 0; i--) {
@@ -32,39 +29,68 @@ public class Main {
 
         // Đẩy lên (để thêm mới)
         void heapifyUp(int childIndex) {
-            int parentIndex = (childIndex - 1) / 2;
-            while (childIndex > 0 && a[childIndex] > a[parentIndex]) {
-                swap(a, childIndex, parentIndex);
-                childIndex = parentIndex;
-                parentIndex = (childIndex - 1) / 2;
-            }
+
         }
 
         // Chèn: O(log n)
         void insert(int value) {
-            a[size] = value;
-            heapifyUp(size);
-            size++;
+
+        }
+
+        void swap(int[] a, int i, int j) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
         }
 
         // Đẩy xuống (heapify xuống để xóa)
-        void siftDown(int parent) {
+//        void siftDown(int parent) {
+//            while (true) {
+//                // parent = 15 -> largest = parent = 15
+//                // left = 78 -> 15 < 78 -> largest = left = 78
+//                // right = 78 ->
+//                // largest = 78
+//                int left = 2 * parent + 1, right = 2 * parent + 2, largest = parent;
+//                if (left < size && a[left] > a[largest]) largest = left; // So sánh largest với left
+//                if (right < size && a[right] > a[largest]) largest = right; // So sánh largest với right
+//                if (largest == parent) break; // duừng vòng lặp nếu parent = giá trij max (> left > right)
+//                swap(a, parent, largest); // Đảo chỗ soo 78 (largest) cho so 15(parent)
+//                parent = largest;
+//            }
+//        }
+        void siftDown(int parent) { // (int i)
             while (true) {
-                int left = 2 * parent + 1, right = 2 * parent + 2, largest = parent;
-                if (left < size && a[left] > a[largest]) largest = left;
-                if (right < size && a[right] > a[largest]) largest = right;
+                int left = 2 * parent + 1, right = 2 * parent + 2;
+                int largest = parent;
+                // thay doi vi tri
+                if (left < size && a[largest] < a[left]) {
+                    largest = left; // 78
+                }
+
+                if (right < size && a[largest] < a[right]) {
+                    largest = right; // 81
+                }
+
+                // if - else if => Chọn 1 trong 2 điều kiện
+                //   65
+                // left: 78 right = 81
+                // 90 -> largest = 90
+                // left 78 right 81
                 if (largest == parent) break;
-                swap(a, parent, largest);
+
+                swap(a, parent, largest); // thay doi phan tu
                 parent = largest;
             }
         }
 
         // Lấy max: O(log n)
         int extractMax() {
-            int max = a[0];
-            a[0] = a[size - 1];
+            int max = a[0]; // gan max voi phan tu dau tien tai vi tri 0 (xoa phan tu tren cung)
+            a[0] = a[size - 1];// gan phan tu dau tien = phan tu cuoi cung.
+            // phan tu cuoi cung = chieu dai - 1
             size--;
-            if (size > 0) siftDown(0);
+            siftDown(0); // so sanh bang cach doi vi tri va so phan tu cua max
+            // thoa man max la so phan tu cao nhat o vi tri 0
             return max;
         }
 
@@ -84,13 +110,13 @@ public class Main {
         System.out.println("Extract max: " + max1);
         System.out.println("Heap sau: " + heap);
 
-        int max2 = heap.extractMax();
-        System.out.println("Extract max: " + max2);
-        System.out.println("Heap sau: " + heap);
-
-        // Demo insert
-        heap.insert(40);
-        heap.insert(5);
-        System.out.println("Sau khi insert 40 và 5: " + heap);
+//        int max2 = heap.extractMax();
+//        System.out.println("Extract max: " + max2);
+//        System.out.println("Heap sau: " + heap);
+//
+//        // Demo insert
+//        heap.insert(40);
+//        heap.insert(5);
+//        System.out.println("Sau khi insert 40 và 5: " + heap);
     }
 }
